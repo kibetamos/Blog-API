aa7b933f9b1a34dd6c80672e46dbc1079d17b326
kibet kilel <amos.rotich@strathmore.edu>
2022-04-25 09:10:31 +0300

    Update

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 # new
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
